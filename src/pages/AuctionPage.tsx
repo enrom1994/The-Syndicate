36c4c2b7fa2faa372b309b4011505d022da50342
enrom1994 <enrom.strydom@gmail.com>
2025-12-10 00:57:53 +0200

    fix: AuctionPage syntax error - remove extra closing brace

--- src/pages/AuctionPage.tsx
+++ src/pages/AuctionPage.tsx
@@ -356,7 +356,7 @@ const AuctionPage = () => {
                                             </Button>
                                         </div>
                                     </motion.div>
-                                ))}
+                                ))
                             )}
                         </TabsContent>
 
